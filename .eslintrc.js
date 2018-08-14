decc9d187ff6e963ca0e96fc4df651e8b2a4e4bd
David <da70@nyu.edu>
2018-08-14 12:44:16 -0400

    .eslintrc.js: turn off `no-use-before-define` [+]
    See https://github.com/babel/babel-eslint/issues/517

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,8 @@ module.exports = {
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // https://github.com/babel/babel-eslint/issues/517
+    'no-use-before-define': 'off',
     // Always terminate statements with semi-colons
     'semi': [ 'error', 'always' ],
   }
