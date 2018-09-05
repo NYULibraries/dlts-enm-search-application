9627e55376f2c2bcb14be1d1b5d4c9e33d8210ca
David <da70@nyu.edu>
2018-09-05 13:26:16 -0400

    Rename tests/e2e/ -> tests/browser/

--- test/browser/conf/wdio.main.conf.js
+++ test/browser/conf/wdio.main.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/e2e/tests/**/*.js',
+        './test/browser/tests/**/*.js',
     ],
     // Patterns to exclude.
     exclude: [
@@ -74,7 +74,7 @@ exports.config = {
     bail: 0,
     //
     // Saves a screenshot to a given path if a command fails.
-    screenshotPath: './test/e2e/errorShots/',
+    screenshotPath: './test/browser/errorShots/',
     //
     // Set a base URL in order to shorten url command calls. If your `url` parameter starts
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
