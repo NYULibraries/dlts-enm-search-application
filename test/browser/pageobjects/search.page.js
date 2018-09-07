d9ed20403ab847e1436e28f2d82fe5502e7c406f
David <da70@nyu.edu>
2018-09-07 18:20:05 -0400

    search.page.js: add baseUrl, path, and navbar getters

--- test/browser/pageobjects/search.page.js
+++ test/browser/pageobjects/search.page.js
@@ -1,15 +1,31 @@
-/* global browser:false */
+/* global browser:false $:false */
 
 import Page from './page';
 
 class SearchPage extends Page {
+    get baseUrl() { return browser.options.baseUrl; }
+    get path() { return 'prototypes/search-results'; }
+    get navbar() {
+        return {
+            about: $( 'a.navbar-item=About' ),
+
+            // Menu
+            browse: $( 'a.navbar-link=Browse' ),
+            // First item of Browse menu
+            featuredTopics: $( 'a.navbar-item=Featured topics' ),
+            // Second item of Browse menu
+            allTopics: $( 'a.navbar-item=All topics' ),
+
+            search: $( 'a.navbar-item=Search' ),
+        };
+    }
     get title() { return browser.getTitle(); };
 
     open() {
         // This is the real path, after the build is implemented.
         // super.open( 'search' );
         // For now use the prototype for writing of the tests.
-        super.open( 'prototypes/search-results' );
+        super.open( this.path );
     }
 }
 
