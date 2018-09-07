b03168d5caeb59641c743de7f776b6936bda8aa8
David <da70@nyu.edu>
2018-09-07 18:57:42 -0400

    navbar tests: add setup() and new tess for About, Browse, All topics, and Featured topics

--- test/browser/tests/navbar.js
+++ test/browser/tests/navbar.js
@@ -1,10 +1,44 @@
-/* global suite:false test:false */
+/* global setup:false suite:false test:false */
 
 import { assert } from 'chai';
 
 import SearchPage from '../pageobjects/search.page';
 
 suite( 'Navbar', function () {
+    setup( function () {
+        SearchPage.open();
+    } );
+
+    test( 'Clicking "About" takes user to About page', function () {
+        SearchPage.open();
+        SearchPage.navbar.about.click();
+
+        assert.equal(  SearchPage.currentUrl, SearchPage.baseUrl + SearchPage.paths.about );
+    } );
+
+    test( 'Clicking "Browse" takes user to browse topics lists', function () {
+        SearchPage.open();
+        SearchPage.navbar.browse.click();
+
+        assert.equal(  SearchPage.currentUrl, SearchPage.baseUrl + SearchPage.paths.browse );
+    } );
+
+    test( 'Clicking "All topics" under "Browse" menu takes user to all topics', function () {
+        SearchPage.open();
+        SearchPage.expandNavbarBrowseMenu();
+        SearchPage.navbar.allTopics.click();
+
+        assert.equal(  SearchPage.currentUrl, SearchPage.baseUrl + SearchPage.paths.allTopics );
+    } );
+
+    test( 'Clicking "Featured topics" under "Browse" menu takes user to featured topics', function () {
+        SearchPage.open();
+        SearchPage.expandNavbarBrowseMenu();
+        SearchPage.navbar.featuredTopics.click();
+
+        assert.equal(  SearchPage.currentUrl, SearchPage.baseUrl + SearchPage.paths.featuredTopics );
+    } );
+
     test( 'Clicking "Search" takes user to search application', function () {
         SearchPage.open();
         SearchPage.navbar.search.click();
