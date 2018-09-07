f9113eedd524315af008d2dbc924113ebf5794c2
David <da70@nyu.edu>
2018-09-07 18:28:24 -0400

    wdio.main.conf.js: temporarily disable Firefox testing while writing tests

--- test/browser/conf/wdio.main.conf.js
+++ test/browser/conf/wdio.main.conf.js
@@ -69,14 +69,15 @@ exports.config = {
             //
             browserName: 'chrome',
         },
-        {
-            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-            // grid with only 5 firefox instances available you can make sure that not more than
-            // 5 instances get started at a time.
-            maxInstances: 5,
-            //
-            browserName: 'firefox',
-        },
+        // TODO: Uncomment when ready to test Firefox
+        // {
+        //     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+        //     // grid with only 5 firefox instances available you can make sure that not more than
+        //     // 5 instances get started at a time.
+        //     maxInstances: 5,
+        //     //
+        //     browserName: 'firefox',
+        // },
     ],
     //
     // ===================
