b47a2b1116fcffe3de355b2d4270973b18c412b2
David <da70@nyu.edu>
2018-09-08 17:54:32 -0400

    test/browser/tests/searchForm.js: initial commit

---
+++ test/browser/tests/searchForm.js
@@ -0,0 +1,25 @@
+/* global setup:false suite:false test:false */
+
+import { assert } from 'chai';
+
+import SearchPage from '../pageobjects/search.page';
+
+suite( 'Search form', function () {
+    setup( function () {
+        SearchPage.open();
+    } );
+
+    test( '"Full Text" checkbox is checked by default', function () {
+        assert(
+            SearchPage.searchForm.fulltextCheckbox.isSelected(),
+            '"Full Text" checkbox is not checked'
+        );
+    } );
+
+    test( '"Topics" checkbox is checked by default', function () {
+        assert(
+            SearchPage.searchForm.topicsCheckbox.isSelected(),
+            '"Topics" checkbox is not checked'
+        );
+    } );
+} );
