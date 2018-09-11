65abe711c60a3cb5166e23ed71971f6f73ae5984
David <da70@nyu.edu>
2018-09-11 17:54:28 -0400

    wdio.main.conf.js: increase waitforTimeout from 10000 30000

--- test/browser/conf/wdio.main.conf.js
+++ test/browser/conf/wdio.main.conf.js
@@ -125,7 +125,7 @@ exports.config = {
     baseUrl: 'http://localhost/enm/search/',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 30000,
     //
     // Default timeout in milliseconds for request
     // if Selenium Grid doesn't send response
