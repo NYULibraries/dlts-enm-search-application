1a8a0ee9dea60db9814920a20eca8b5ac2ce4cc9
David <da70@nyu.edu>
2018-09-10 21:16:24 -0400

    limit-by-topic-pane: temporary driver code

--- test/browser/tests/limit-by-topic-pane.js
+++ test/browser/tests/limit-by-topic-pane.js
@@ -14,6 +14,22 @@ suite( 'Navbar', function () {
 
         let topicsWithHitCounts = SearchPage.limitByTopicPane.topicsWithHitCounts;
 
+        let topic = topicsWithHitCounts[ 0 ][ 0 ];
+
+        SearchPage.limitByTopicPane.click( topic );
+        SearchPage.limitByTopicPane.click( 'Andreas-Salomé, Lou (Luise)' );
+        SearchPage.limitByTopicPane.click( 'Disintegration -- fear of' );
+
+        console.log( SearchPage.searchEcho.searchDCI.query );
+        console.log( SearchPage.searchEcho.topicDCIs.topics );
+
+        SearchPage.searchEcho.topicDCIs.dismiss( topic );
+        console.log( SearchPage.searchEcho.topicDCIs.topics );
+
+        SearchPage.searchEcho.searchDCI.dismiss();
+
+        console.log( SearchPage.searchEcho.topicDCIs.topics );
+
         console.log( topicsWithHitCounts );
     } );
 } );
