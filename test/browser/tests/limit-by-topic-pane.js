26cd871bf8c6d48a00d7d9a5217cf2f1601bf058
David <da70@nyu.edu>
2018-09-26 22:04:22 -0400

    limit-by-topic-pane test: first draft of "Clicking "See all" link produces correct full topics list (not working because LimitByTopicPane API needs to be upgraded)

--- test/browser/tests/limit-by-topic-pane.js
+++ test/browser/tests/limit-by-topic-pane.js
@@ -1,8 +1,13 @@
 /* global setup:false suite:false test:false */
 
+import _ from 'lodash';
 import { assert } from 'chai';
-
+import { EOL } from 'os';
 import SearchPage from '../pageobjects/search.page';
+import {
+    getGoldenFilePath,
+    SUITE_NAME,
+} from '../util';
 
 suite( 'Limit by Topic Pane', function () {
     setup( function () {
@@ -32,4 +37,37 @@ suite( 'Limit by Topic Pane', function () {
             '"See all" link is present even when there are no toggle-able topics'
         );
     } );
+
+    test(
+        'Clicking "See all" link produces correct full topics list',
+        function () {
+            SearchPage.searchAndWaitForResults( 'Dungeons & Dragons' );
+            SearchPage.limitByTopicPane.seeAllLink.click();
+
+            const expectedLimitByTopics = require(
+                getGoldenFilePath( SUITE_NAME.limitByTopicPane, SearchPage.getSearchIdForCurrentSearch() )
+            );
+            const actualLimitByTopics = SearchPage.limitByTopicPane.topicNames;
+
+            const inActualNotExpected = _.difference( actualLimitByTopics, expectedLimitByTopics );
+            const inExpectedNotActual = _.difference( expectedLimitByTopics, actualLimitByTopics );
+
+            const ok = ( inActualNotExpected.length === 0 && inExpectedNotActual.length === 0 );
+            let message = '';
+            if ( ! ok ) {
+                if ( inActualNotExpected.length > 0 ) {
+                    message += 'In actual not expected: ' +
+                               JSON.stringify( inActualNotExpected, null, '   ' ) +
+                               EOL;
+                }
+
+                if ( inExpectedNotActual.length > 0 ) {
+                    message += 'In expected not actual: ' +
+                               JSON.stringify( inExpectedNotActual, null, '   ' );
+                }
+            }
+
+            assert( ok, message );
+        }
+    );
 } );
