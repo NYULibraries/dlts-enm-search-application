60231dce5a2b1081ec2e73750b49bd208bc8a2bc
David <da70@nyu.edu>
2018-09-26 20:48:45 -0400

    limit-by-topic-pane tests: new test "Topics already selected will not appear in the Limit by Topic list"

--- test/browser/tests/limit-by-topic-pane.js
+++ test/browser/tests/limit-by-topic-pane.js
@@ -8,4 +8,19 @@ suite( 'Limit by Topic Pane', function () {
     setup( function () {
         SearchPage.open();
     } );
+
+    test( 'Topics already selected will not appear in the Limit by Topic list', function () {
+        SearchPage.searchAndWaitForResults( 'art' );
+
+        const testTopic = SearchPage.limitByTopicPane.topicNames[ 0 ];
+
+        SearchPage.limitByTopicPane.topic( testTopic ).click();
+
+        const newLimitByTopicList = SearchPage.limitByTopicPane.topicNames;
+
+        assert(
+            newLimitByTopicList.includes( testTopic ) === false,
+            `"${testTopic}" appears in new Limit by Topic after it has already been selected`
+        );
+    } );
 } );
