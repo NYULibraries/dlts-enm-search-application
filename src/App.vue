ad90b1314514e11419c9f5006e63ab6354815889
David <da70@nyu.edu>
2018-11-10 12:06:17 -0500

    App BUGFIX: bind PreviewPane isbn prop

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
                     />
                     <preview-pane
                         :display="previewPane.display"
-                        isbn="previewPane.isbn"
+                        :isbn="previewPane.isbn"
                     />
                 </div>
             </div>
