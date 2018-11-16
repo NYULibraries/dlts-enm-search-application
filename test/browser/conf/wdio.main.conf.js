99ef5b447d28057042c90f31799ccc52e2d668ba
David <da70@nyu.edu>
2018-11-16 11:20:54 -0500

    wdio.main.conf.js: fix baseUrl

--- test/browser/conf/wdio.main.conf.js
+++ test/browser/conf/wdio.main.conf.js
@@ -140,7 +140,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost/enm/search/',
+    baseUrl: 'http://localhost/enm/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 30000,
