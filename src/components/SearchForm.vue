8f6574c10d8ac163722b39aee632fc9d551f4dd5
David <da70@nyu.edu>
2018-11-27 11:54:37 -0500

    SearchForm: add query watcher

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -82,6 +82,22 @@ export default {
             ]
         ),
     },
+    watch : {
+        query( newValue, oldValue ) {
+            // This watcher is for updating the form when another component
+            // changes the query.  Note that this watcher forms a loop in the case of
+            // the user submitting a new query via this component's own form,
+            // since the submit form code changes the query.  It's a very fast
+            // and finite loop however, and there may in fact come a time when in
+            // the loop we would want to change the queryUI value -- for example,
+            // for stopword removal or some other query rewrite that happens on
+            // submission.
+            // The loop could be broken by moving queryUI from data to props and
+            // having App set it when another component changes the query, but
+            // doing that doesn't seem worth the added complexity.
+            this.queryUI = newValue;
+        },
+    },
     methods: {
         ...mapActions(
             [
