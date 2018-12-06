abdc903757f0943624b260d645d84a97dc2a9f50
David <da70@nyu.edu>
2018-12-05 21:11:24 -0500

    config/dev.env.js: add SOLR environment variables

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,4 +4,7 @@ const prodEnv = require( './prod.env' );
 
 module.exports = merge( prodEnv, {
     NODE_ENV: '"development"',
+    SOLR_HOST: '"dev-discovery.dlib.nyu.edu"',
+    SOLR_PORT: '8983',
+    SOLR_PROTOCOL: '"http"',
 } );
