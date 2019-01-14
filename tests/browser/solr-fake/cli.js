d18c49300b5fee716f42a09903912eb6bba0785a
David <da70@nyu.edu>
2019-01-14 17:45:28 -0500

    tests/browser/solr-fake/cli.js: resolve pathnames for solrResponsesDirectory and solrRequestRewriterMiddleware

--- tests/browser/solr-fake/cli.js
+++ tests/browser/solr-fake/cli.js
@@ -1,15 +1,19 @@
+const fs = require( 'fs' );
+
 const argv = require( 'minimist' )( process.argv.slice( 2 ) );
 
 const solrFake = require( './' );
 
-const solrResponsesDirectory = argv._.pop();
-
-let solrRequestRewriterMiddleware;
+let solrResponsesDirectory = argv._.pop();
+if ( solrResponsesDirectory ) {
+    solrResponsesDirectory = fs.realpathSync( solrResponsesDirectory );
+}
 
 let port = argv.port || undefined;
 
+let solrRequestRewriterMiddleware;
 if ( argv.middleware ) {
-    solrRequestRewriterMiddleware = require( argv.middleware );
+    solrRequestRewriterMiddleware = require( fs.realpathSync( argv.middleware ) );
 }
 
 solrFake.startSolrFake( solrResponsesDirectory, port, solrRequestRewriterMiddleware );
