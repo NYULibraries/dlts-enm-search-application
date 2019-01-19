9dd7419f3d8376d94cb2875c925816767521a9ca
David <da70@nyu.edu>
2019-01-19 14:14:01 -0500

    SearchPage: remove obsolete comment

--- tests/browser/pageobjects/search.page.js
+++ tests/browser/pageobjects/search.page.js
@@ -51,9 +51,6 @@ class SearchPage extends Page {
     };
 
     open() {
-        // This is the real path, after the build is implemented.
-        // super.open( 'search' );
-        // For now use the prototype for writing of the tests.
         super.open( this.paths.search + `?solr=${ browser.options.solrFake.url }` );
     }
 
