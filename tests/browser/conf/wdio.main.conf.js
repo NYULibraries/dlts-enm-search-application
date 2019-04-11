747feafa6e4346b67d488d1857bbac8a83a3f322
David <da70@nyu.edu>
2019-04-11 14:00:32 -0400

    wdio.main.conf.js: change chromeOptions -> goog:chromeOptions

--- tests/browser/conf/wdio.main.conf.js
+++ tests/browser/conf/wdio.main.conf.js
@@ -93,7 +93,7 @@ exports.config = {
             // maxInstances: 5,
             //
             browserName   : 'chrome',
-            chromeOptions : {
+            'goog:chromeOptions' : {
                 // to run chrome headless the following flags are required
                 // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
                 args : [ '--headless' ],
