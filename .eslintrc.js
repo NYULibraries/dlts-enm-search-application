2678d3acdea2a7ff1b7de2bd33000601952af775
David <da70@nyu.edu>
2019-06-13 16:02:57 -0400

    .eslintrc.js: whitespace reformatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,13 +10,13 @@ module.exports = {
         // With import/export syntax
         'sourceType'  : 'module',
     },
-    env : {
-        browser : true,
-        es6     : true,
-        'jest/globals': true,
-        node    : true,
+    env           : {
+        browser        : true,
+        es6            : true,
+        'jest/globals' : true,
+        node           : true,
     },
-    extends : [
+    extends       : [
         // https://github.com/jest-community/eslint-plugin-jest/blob/master/src/index.js?#L19-#L37
         'plugin:jest/recommended',
         // https://github.com/jest-community/eslint-plugin-jest/blob/master/src/index.js?#L38-#L46
@@ -27,17 +27,17 @@ module.exports = {
         'plugin:vue/recommended',
     ],
     // required to lint *.vue files
-    plugins : [
+    plugins       : [
         'jest',
         'vue',
     ],
     // add your custom rules here
-    rules : {
-        'comma-dangle'           : [ 'error', 'always-multiline' ],
+    rules         : {
+        'comma-dangle'                            : [ 'error', 'always-multiline' ],
         // allow async-await
-        'generator-star-spacing' : 'off',
-        'indent'                 : [ 'warn', 4 ],
-        'key-spacing'            : [
+        'generator-star-spacing'                  : 'off',
+        'indent'                                  : [ 'warn', 4 ],
+        'key-spacing'                             : [
             'error',
             {
                 'beforeColon' : true,
@@ -51,7 +51,7 @@ module.exports = {
                 },
             },
         ],
-        'keyword-spacing' : [
+        'keyword-spacing'                         : [
             'error',
             {
                 'overrides' : {
@@ -59,23 +59,25 @@ module.exports = {
                 },
             },
         ],
-        'no-console'                  : process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-console'                              : process.env.NODE_ENV === 'production' ? 'error' : 'off',
         // allow debugger during development
-        'no-debugger'                 : process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-multi-spaces'             : 'off',
+        'no-debugger'                             : process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-multi-spaces'                         : 'off',
         // https://github.com/babel/babel-eslint/issues/517
-        'no-use-before-define'        : 'off',
-        'one-var'                     : [ 'error', { var : 'always' } ],
+        'no-use-before-define'                    : 'off',
+        'one-var'                                 : [ 'error', { var : 'always' } ],
         // Always terminate statements with semi-colons
-        'semi'                        : [ 'error', 'always' ],
-        'space-before-function-paren' : [ 'error',
-            { 'anonymous'  : 'always',
+        'semi'                                    : [ 'error', 'always' ],
+        'space-before-function-paren'             : [
+            'error',
+            {
+                'anonymous'  : 'always',
                 'named'      : 'never',
                 'asyncArrow' : 'always',
             },
         ],
-        'space-in-parens' : [ 'error', 'always' ],
-        'space-unary-ops' : [
+        'space-in-parens'                         : [ 'error', 'always' ],
+        'space-unary-ops'                         : [
             'error',
             {
                 'words'     : true,
@@ -95,23 +97,26 @@ module.exports = {
                 'closeBracket'              : 0,
                 'alignAttributesVertically' : true,
                 'ignores'                   : [],
-            } ],
-        'vue/html-self-closing' : ['error', {
-            'html' : {
-                'void'      : 'never',
-                'normal'    : 'never',
-                'component' : 'always',
-            },
-            'svg'  : 'always',
-            'math' : 'always',
-        } ],
-        'vue/html-quotes'                    : 'error',
-        'vue/max-attributes-per-line'        : 'error',
-        'vue/mustache-interpolation-spacing' : 'error',
-        'vue/name-property-casing'           : 'error',
-        'vue/require-default-prop'           : 'error',
-        'vue/require-prop-types'             : 'error',
-        'vue/v-bind-style'                   : 'error',
-        'vue/v-on-style'                     : 'error',
+            }
+        ],
+        'vue/html-self-closing'                   : [
+            'error', {
+                'html' : {
+                    'void'      : 'never',
+                    'normal'    : 'never',
+                    'component' : 'always',
+                },
+                'svg'  : 'always',
+                'math' : 'always',
+            }
+        ],
+        'vue/html-quotes'                         : 'error',
+        'vue/max-attributes-per-line'             : 'error',
+        'vue/mustache-interpolation-spacing'      : 'error',
+        'vue/name-property-casing'                : 'error',
+        'vue/require-default-prop'                : 'error',
+        'vue/require-prop-types'                  : 'error',
+        'vue/v-bind-style'                        : 'error',
+        'vue/v-on-style'                          : 'error',
     },
 };
