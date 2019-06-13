f563b522c36e2cf86f2b88d0d4db3acbf1dd4548
David <da70@nyu.edu>
2019-06-13 17:37:35 -0400

    Extract src/store/store-config.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,62 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import storeConfig from './store-config';
+
 Vue.use( Vuex );
 
-const store = new Vuex.Store( {
-    strict : process.env.NODE_ENV !== 'production',
-
-    state() {
-        return {
-            query                   : null,
-            queryFields             : [],
-            selectedTopicFacetItems : [],
-        };
-    },
-    getters : {
-        query : state => state.query,
-
-        queryFields : state => state.queryFields,
-
-        selectedTopicFacetItems : state => state.selectedTopicFacetItems,
-    },
-    mutations : {
-        addSelectedTopicFacetItem( state, topicFacetItem ) {
-            state.selectedTopicFacetItems.push( topicFacetItem );
-        },
-        clearSelectedTopicFacetItems( state ) {
-            state.selectedTopicFacetItems = [];
-        },
-        removeSelectedTopicFacetItem( state, topicFacetItem ) {
-            const index = state.selectedTopicFacetItems.indexOf( topicFacetItem );
-            if ( index > -1 ) {
-                state.selectedTopicFacetItems.splice( index, 1 );
-            }
-        },
-        setQuery( state, query ) {
-            state.query = query;
-        },
-        setQueryFields( state, queryFields ) {
-            state.queryFields = queryFields;
-        },
-    },
-    actions : {
-        addSelectedTopicFacetItem( { commit }, topicFacetItem ) {
-            commit( 'addSelectedTopicFacetItem', topicFacetItem );
-        },
-        clearSelectedTopicFacetItems( { commit } ) {
-            commit( 'clearSelectedTopicFacetItems' );
-        },
-        removeSelectedTopicFacetItem( { commit }, topicFacetItem ) {
-            commit( 'removeSelectedTopicFacetItem', topicFacetItem );
-        },
-        setQuery( { commit }, query ) {
-            commit( 'setQuery', query );
-        },
-        setQueryFields( { commit }, queryFields ) {
-            commit( 'setQueryFields', queryFields );
-        },
-    },
-} );
+const store = new Vuex.Store( storeConfig );
 
 export default store;
