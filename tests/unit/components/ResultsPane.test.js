22f27ec838a27ec2f9687dd509b819d7d1699d39
David <da70@nyu.edu>
2019-07-15 16:00:59 -0400

    ResultsPane tests: "renders not results found correctly" test

--- tests/unit/components/ResultsPane.test.js
+++ tests/unit/components/ResultsPane.test.js
@@ -1,5 +1,39 @@
-describe( 'Sanity check', () => {
-    test( 'true is truthy', () => {
-        expect( true ).toBeTruthy();
+import ResultsPane from '@/components/ResultsPane';
+
+import merge from 'lodash.merge';
+import { shallowMount } from '@vue/test-utils';
+
+function createWrapper( overrides ) {
+    const defaultMountingOptions = {
+        propsData : {
+            display  : false,
+            error    : false,
+            numBooks : 0,
+            numPages : 0,
+            results  : [],
+        },
+    };
+
+    return shallowMount( ResultsPane, merge( defaultMountingOptions, overrides ) );
+}
+
+describe( 'ResultsPane', () => {
+    let wrapper;
+
+    beforeEach( () => {
+        wrapper = createWrapper();
+    } );
+
+    test( 'renders no results found correctly', () => {
+        wrapper.setProps(
+            {
+                display  : true,
+                numBooks : 0,
+                numPages : 0,
+                results  : [],
+            }
+        );
+
+        expect( wrapper.element ).toMatchSnapshot();
     } );
 } );
