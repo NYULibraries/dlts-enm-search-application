6bed56a00cb7cb58ad30413dad6e4ed2ad08c94d
David <da70@nyu.edu>
2019-07-18 13:33:52 -0400

    Spinner tests: initial commit

--- tests/unit/components/Spinner.test.js
+++ tests/unit/components/Spinner.test.js
@@ -1,5 +1,33 @@
-describe( 'Sanity check', () => {
-    test( 'true is truthy', () => {
-        expect( true ).toBeTruthy();
+import Spinner from '@/components/Spinner';
+
+import { shallowMount } from '@vue/test-utils';
+
+describe( 'Spinner', () => {
+    let wrapper;
+
+    beforeEach( () => {
+        wrapper = shallowMount( Spinner, {
+            propsData : {
+                display : false,
+            },
+        } );
+    } );
+
+    test( 'displays and hides correctly', () => {
+        expect( wrapper.isVisible() ).toBeFalsy();
+
+        wrapper.setProps( { display : true } );
+
+        expect( wrapper.isVisible() ).toBeTruthy();
+
+        wrapper.setProps( { display : false } );
+
+        expect( wrapper.isVisible() ).toBeFalsy();
+    } );
+
+    test( 'renders correctly when displayed', () => {
+        wrapper.setProps( { display : true } );
+
+        expect( wrapper.element ).toMatchSnapshot();
     } );
 } );
