b2ff635f5ffc0b3f3149714e804b51c564b257b0
David <da70@nyu.edu>
2019-07-24 17:51:36 -0400

    enm-solr.test.js: test that plugin install adds $solr* methods to prototype

--- tests/unit/plugins/enm-solr.test.js
+++ tests/unit/plugins/enm-solr.test.js
@@ -1,5 +1,32 @@
-describe( 'Sanity check', () => {
-    test( 'true is truthy', () => {
-        expect( true ).toBeTruthy();
+import { createLocalVue } from '@vue/test-utils';
+import EnmSolr from '@/plugins/enm-solr';
+
+describe( 'enm-solr plugin', () => {
+    const ENM_SOLR_OPTIONS = Object.freeze(
+        {
+            solrCorePath : '/solr/enm-pages/',
+            solrHost     : 'discovery.dlib.nyu.edu',
+            solrPort     : 8983,
+            solrProtocol : 'http',
+        } );
+
+    let localVue;
+
+    beforeEach( () => {
+        localVue = createLocalVue();
+
+        localVue.use( EnmSolr, ENM_SOLR_OPTIONS );
+    } );
+
+    test( 'adds $solrPreviewEpub to the Vue prototype', () => {
+        expect( typeof localVue.prototype.$solrPreviewEpub ).toBe( 'function' );
+    } );
+
+    test( 'adds $solrPreviewPage to the Vue prototype', () => {
+        expect( typeof localVue.prototype.$solrPreviewPage ).toBe( 'function' );
+    } );
+
+    test( 'adds $solrSearch to the Vue prototype', () => {
+        expect( typeof localVue.prototype.$solrSearch ).toBe( 'function' );
     } );
 } );
