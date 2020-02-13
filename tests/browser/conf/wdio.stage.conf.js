883ecb47673a74bbbfc41cc0ffa9a3621b854008
David <da70@nyu.edu>
2020-02-13 16:36:49 -0500

    tests/browser/conf/wdio.{dev,stage,prod}.conf.js: use new enm{-dev,-stage}.dlib.nyu.edu domain names

--- tests/browser/conf/wdio.stage.conf.js
+++ tests/browser/conf/wdio.stage.conf.js
@@ -4,6 +4,6 @@ let merge        = require( 'deepmerge' );
 let wdioMainConf = require( './wdio.main.conf.js' );
 
 exports.config = merge( wdioMainConf.config, {
-    baseUrl            : 'http://stageweb1.dlib.nyu.edu/',
+    baseUrl            : 'http://enm-stage.dlib.nyu.edu/',
     enmGoogleAnalytics : false,
 } );
